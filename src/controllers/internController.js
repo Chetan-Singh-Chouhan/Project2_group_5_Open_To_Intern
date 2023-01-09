e70981ad40b7aae275dac8edbcab2e374d9f8ecb
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2023-01-09 23:29:29 +0530

    Purushottam

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,6 +1,37 @@
 const internModel = require('../models/internModel')
 
+const createIntern = async function (req, res) {
+    try {
+        let data = req.body;
+        let { name, email, mobile, collegeName } = data;
+        if (!name) return res.status(400).send({ status: false, message: "Please enter your name." });
+        name = name.trim();
+        if (!isValid.name(name)) return res.status(400).send({ status: false, message: "Please enter a valid name." });
+        if (!collegeName) return res.status(400).send({ status: false, message: "Please enter collge name." });
+        collegeName = collegeName.trim();
+        if (!isValid.unabbreviated(collegeName)) return res.status(400).send({ status: false, message: "Please enter a valid unabbreviated college name." });
+        const college = await collegeModel.findOne({ name: collegeName.toLowerCase(), isDeleted: false });
+        if (!college) return res.status(404).send({ status: false, message: "This college is not added for internship." });
+        if (!email) return res.status(400).send({ status: false, message: "Plese enter email address." });
+        email = email.trim();
+        if (!isValid.email(email)) return res.status(400).send({ status: false, message: "Please enter a valid email address." });
+        if (!mobile) return res.status(400).send({ status: false, message: "Please enter mobile number" });
+        mobile = mobile.trim();
+        if (!isValid.mobile(mobile)) return res.status(400).send({ status: false, message: "Please enter 10 digit valid mobile number." });
+        const intern = await internModel.findOne({ $or: [{ mobile: mobile }, { email: email }] });
+        if (intern) {
+            if (intern.email == email) return res.status(400).send({ status: false, message: "This email is already in use." });
+            if (intern.mobile == mobile) return res.status(400).send({ status: false, message: "This mobile number is already in use." });
+        }
+        const newIntern = await internModel.create({ name: name, email: email, mobile: mobile, collegeId: college._id });
+        return res.status(201).send({
+            status: true,
+            data: { isDeleted: false, name: newIntern.name, email: newIntern.email, mobile: newIntern.mobile, collegeId: newIntern.collegeId }
+        });
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
+}
 
 
-
-module.exports={createIntern}
+module.exports = { createIntern }
